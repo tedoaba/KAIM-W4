eee753257769d85c5b407893a4d9cbf17c574e24
tedoaba <tedoaba@gmail.com>
2024-09-20 16:11:01 +0300

    Exploratory Data Analysis

--- src/eda.py
+++ src/eda.py
@@ -1,13 +1,47 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import logging
 
-def explore_data(df):
-    # Example plot - Sales by Promo
-    plt.figure(figsize=(10,6))
-    sns.barplot(x="Promo", y="Sales", data=df)
-    plt.title("Sales vs Promo")
-    plt.savefig('plots/sales_vs_promo.png')
-    
-    logging.info("EDA completed and plots saved successfully")
-    return
+def data_overview(df):
+    logging.info("Generating data overview...")
+    print(f"Dataset Shape: {df.shape}")
+    print(f"Columns: {df.columns}")
+    print(df.info())
+    print(df.describe())
+
+def plot_store_type_distribution(df):
+    plt.figure(figsize=(8, 6))
+    sns.countplot(x='StoreType', data=df)
+    plt.title("Distribution of Stores by StoreType")
+    plt.show()
+
+def plot_assortment_distribution(df):
+    plt.figure(figsize=(8, 6))
+    sns.countplot(x='Assortment', data=df)
+    plt.title("Distribution of Stores by Assortment")
+    plt.show()
+
+def plot_competition_distance_distribution(df):
+    plt.figure(figsize=(10, 6))
+    sns.histplot(df['CompetitionDistance'], kde=True)
+    plt.title('Distribution of CompetitionDistance')
+    plt.show()
+
+def plot_promo2_distribution(df):
+    plt.figure(figsize=(8, 6))
+    sns.countplot(x='Promo2', data=df)
+    plt.title("Promo2 Distribution")
+    plt.show()
+
+def plot_competition_distance_by_assortment(df):
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(x='Assortment', y='CompetitionDistance', data=df)
+    plt.title('CompetitionDistance vs Assortment')
+    plt.show()
+
+def plot_correlation_matrix(df):
+    plt.figure(figsize=(12, 8))
+    sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+    plt.title("Correlation between features")
+    plt.show()
