185b3ecd93b8bbc42964efa76a036820ce2a786c
tedoaba <tedoaba@gmail.com>
2024-09-20 16:12:01 +0300

    Utils for log

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,4 @@
 import logging
 
-def setup_logging(log_file):
-    logging.basicConfig(filename=log_file, level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(message)s')
+def setup_logging():
+    logging.basicConfig(filename="../logs/eda.log", level=logging.INFO)
