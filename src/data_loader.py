d992149a00e59ce2ad3fa2290e45dd558321f409
tedoaba <tedoaba@gmail.com>
2024-09-20 20:18:15 +0300

    Store train and test data loaded separately

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import logging
 
-def load_data(file_path):
-    logging.info(f"Loading data from {file_path}")
-    return pd.read_csv(file_path)
+# Function to load datasets
+def load_data(train_path, test_path, store_path):
+    logging.info(f"Loading data from {train_path}, {test_path}, {store_path}")
+    train = pd.read_csv(train_path)
+    test = pd.read_csv(test_path)
+    store = pd.read_csv(store_path)
+    return train, test, store
